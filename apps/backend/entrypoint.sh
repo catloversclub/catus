0221777336a83d5b68f4ecd575de965d2cfefcde
w0uldy0u <spacekim01@gmail.com>
2025-10-17 03:33:15 +0900

    fix(infra): fix prisma schema not found

--- apps/backend/entrypoint.sh
+++ apps/backend/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "⏳ Running Prisma migrate deploy..."
 until nc -z postgres 5432; do echo "waiting for postgres..."; sleep 2; done
-pnpm dlx prisma migrate deploy
+pnpm dlx prisma migrate deploy --schema ./prisma/schema.prisma
 
 echo "🚀 Starting NestJS API..."
 exec node dist/main.js
