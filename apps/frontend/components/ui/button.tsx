253ae3c85ad956647b4949a56cfa3ab7067ea873
B0XERCAT <seojin3154@naver.com>
2025-10-20 17:40:21 +0900

    fix: fix vercel error

--- apps/frontend/components/ui/button.tsx
+++ apps/frontend/components/ui/button.tsx
@@ -21,11 +21,12 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         icon: "w-[50px] h-[50px] rounded-full border border-border bg-background text-outline-foreground hover:bg-outline-pressed hover:text-outline-pressed-foreground disabled:text-outline-disabled justify-self-center",
       },
-      size: {
-        default: "h-[50px] px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 px-6 has-[>svg]:px-4",
-      },
+        size: {
+          default: "h-[50px] px-4 py-2 has-[>svg]:px-3",
+          sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+          lg: "h-10 px-6 has-[>svg]:px-4",
+          icon: "size-9",
+        },
     },
     defaultVariants: {
       variant: "default",
