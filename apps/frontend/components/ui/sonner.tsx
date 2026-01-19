002605fc18dd69813c8a7b065a1b402f441767b3
B0XERCAT <seojin3154@naver.com>
2026-01-19 17:57:01 +0900

    chore(fe): modify toast style

--- apps/frontend/components/ui/sonner.tsx
+++ apps/frontend/components/ui/sonner.tsx
@@ -1,12 +1,7 @@
 "use client"
 
-import {
-  CircleCheckIcon,
-  InfoIcon,
-  Loader2Icon,
-  OctagonXIcon,
-  TriangleAlertIcon,
-} from "lucide-react"
+import { CircleCheckIcon, InfoIcon, Loader2Icon, TriangleAlertIcon } from "lucide-react"
+import { TiDelete } from "react-icons/ti"
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 
@@ -16,13 +11,23 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      position="bottom-center"
       className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "w-80 px-2.5 py-3 bg-background-secondary rounded shadow-[0px_0px_15px_0px_rgba(0,0,0,0.05)] outline outline-1 outline-offset-[-1px] outline-border",
+          title: "flex-1 text-text-primary text-sm font-normal leading-6",
+          description: "hidden",
+          icon: "relative overflow-hidden shrink-0",
+        },
+      }}
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        success: <CircleCheckIcon />,
+        info: <InfoIcon />,
+        warning: <TriangleAlertIcon />,
+        error: <TiDelete className="text-icon-error size-5" />,
+        loading: <Loader2Icon />,
       }}
       style={
         {
