e04b03e84258dc439f31f2e70154f4988718f60c
Mingyu <minngyuseong@gmail.com>
2026-01-30 01:09:04 +0900

    chore(fe): update prop

--- apps/frontend/app/post-login/page.tsx
+++ apps/frontend/app/post-login/page.tsx
@@ -9,5 +9,5 @@ export default async function PostLoginPage() {
   }
 
   // 서버에서 받은 session 데이터를 클라이언트 컴포넌트로 넘겨줍니다.
-  return <PostLoginClient sessionData={session} />
+  return <PostLoginClient />
 }
